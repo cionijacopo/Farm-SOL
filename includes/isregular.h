48f4a45b648c8269b2c9649122702b96519b2dc6
cionijacopo <cionijacopo4@gmail.com>
2023-05-07 18:32:54 +0200

    Created isRegular file (.c/.h)

---
+++ includes/isregular.h
@@ -0,0 +1,30 @@
+#if !defined(ISREGULAR_H_)
+#define ISREGULAR_H_
+
+#include"./linkedlist.h"
+
+/**
+ * @function is_regular_file
+ * @brief La funzione controlla che il parametro sia un file.
+ * @param file file da controllare
+ * @return 0: successo, 1: non è un file, -1: Errore Stat
+*/
+int is_regular_file(char *file);
+
+/**
+ * @function is_regular_folder
+ * @brief La funzione controlla che il parametro sia una cartella.
+ * @param folder_name cartella da controllare
+ * @return 0: successo, 1: non è una cartella, -1: Errore Stat
+*/
+int is_regular_folder(char *folder_name);
+
+/**
+* @function naviga_cartella
+* @brief funzione ricorsiva che naviga la cartella passata come parametro ed aggiunge i file ad una lista
+* @param folder cartella che viene navigata
+* @param lista dove viene inserito il file
+*/
+void naviga_cartella(char *folder_name, Node_t *lista);
+
+#endif
---
+++ src/core/isregular.c
@@ -0,0 +1,83 @@
+/*
+    Created by Jacopo Cioni
+    FarmProject - SOL
+*/
+
+#include"../../includes/isregular.h"
+#include"../../includes/linkedlist.h"
+#include"../../includes/utils.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<dirent.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int is_regular_file(char *file) {
+    struct stat pathbuf;
+    if(stat(file, &pathbuf) != 0) {
+        // Errore stat
+        return -1;
+    }
+    // Se ritorna zero non è un file
+    if(S_ISREG(pathbuf.st_mode) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int is_regular_folder(char *folder_name) {
+    struct stat pathbuf_f;
+    if(stat(folder_name, &pathbuf_f) != 0) {
+        // Errore stat
+        return -1;
+    }
+    // Se ritorna zero non è un file
+    if(S_ISDIR(pathbuf_f.st_mode) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// stackoverflow code
+void naviga_cartella(char *folder_name, Node_t *lista) {
+    DIR *cartella;
+    if((cartella = opendir(folder_name)) == NULL) {
+        fprintf(stderr, "FATAL ERROR: navigazione della directory.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        char *sub_cartella = NULL;
+        errno = 0;
+        struct dirent *file;
+        // Comincio ad iterare
+
+        while((file = readdir(cartella)) != NULL && (errno == 0)) {
+            if(strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0) {
+                continue;
+            } else {
+                sub_cartella = (char *)s_malloc(strlen(file->d_name)+1);
+                strncpy(sub_cartella, folder_name, strlen(folder_name)+1);
+                strncat(sub_cartella, "/", 2);
+                strncat(sub_cartella, file->d_name, strlen(file->d_name)+1);
+                if(is_regular_folder(sub_cartella) == 0) {
+                    naviga_cartella(sub_cartella, lista);
+                } else {
+                    if(is_regular_file(sub_cartella) == 0) {
+                        // Aggiungiere alla coda
+                        postInsert(lista, sub_cartella);
+                    }
+                }
+            }
+        }
+        if(errno != 0) {
+            fprintf(stderr, "Errore: readdir");
+        } else {
+            closedir(cartella);
+        }
+    }
+}
