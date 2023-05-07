4951cedc36698ad1ba13cb1d4c931149c8a1b05a
cionijacopo <cionijacopo4@gmail.com>
2023-05-07 17:06:56 +0200

    Created connection file (.c/.h)

---
+++ includes/connection.h
@@ -0,0 +1,55 @@
+#if !defined(CONNECTION_H_)
+#define CONNECTION_H_
+
+#include<stdio.h>
+
+#define MAX_BACK_LOG 32
+#define SOCKETNAME "./farm.sck"
+
+/**
+ * @function initServerAddress
+ * @brief funzione che inizializza il server sulla socket
+*/
+void initServerAddress();
+
+/**
+ * @function cleanup
+ * @brief pulizia della SOCKETNAME
+*/
+void cleanup();
+
+/**
+ * @function serverSocket
+ * @brief funzione che restituisce il file descriptor del server connesso ala socket
+ * @return fd_server in caso di successo, -1 in caso di errore
+*/
+int serverSocket();
+
+/**
+ * @function clientSocket
+ * @brief funzione che restituisce il file descriptor del client connesso ala socket
+ * @return fd_client in caso di successo, -1 in caso di errore
+*/
+int clientSocket();
+
+/**
+ * @function readn
+ * @brief funzione di lettura di un buffer di dimensione size, evita letture parziali
+ * @param fd file descriptor 
+ * @param buf buffer
+ * @param size dimensione del buffer
+ * @return 0 se durante la lettura leggo EOF, -1 in caso di errore, size se termina correttamente
+*/
+int readn(long fd, void *buf, size_t size);
+
+/**
+ * @function writen
+ * @brief funzione di scrittura di un buffer di dimensione size, evita scritture parziali
+ * @param fd file descriptor 
+ * @param buf buffer
+ * @param size dimensione del buffer
+ * @return 0 se durante la scrittura la write ritorna 0, -1 in caso di errore, 1 se termina correttamente
+*/
+int writen(long fd, void *buf, size_t size);
+
+#endif
--- src/core/connection.c
+++ src/core/connection.c
@@ -2,3 +2,112 @@
     Created by Jacopo Cioni
     FarmProject - SOL
 */
+
+#include"../../includes/connection.h"
+
+#include<errno.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/un.h>
+#include<sys/socket.h>
+
+struct sockaddr_un socket_address;
+
+void initServerAddress() {
+    memset(&socket_address, '0', sizeof(socket_address));
+    socket_address.sun_family = AF_UNIX;
+    strncpy(socket_address.sun_path, SOCKETNAME, strlen(SOCKETNAME)+1);
+}
+
+void cleanup() {
+    unlink(SOCKETNAME);
+}
+
+int serverSocket(int *fd_socket) {
+    // Ottengo il file descriptor del socket
+    (*fd_socket) = socket(AF_UNIX, SOCK_STREAM, 0);
+    // Controllo che sia tutto ok
+    if((*fd_socket) == -1) {
+        perror("SOCKET");
+        cleanup();
+        return -1;
+    }
+    // Faccio il binding 
+    if((bind((*fd_socket), (struct sockaddr *)&socket_address, sizeof(socket_address))) != 0) {
+        perror("BIND");
+        close((*fd_socket));
+        cleanup();
+        return -1;
+    }
+    if((listen((*fd_socket), MAX_BACK_LOG)) != 0) {
+        perror("LISTEN");
+        close((*fd_socket));
+        cleanup();
+        return -1;
+    }
+    return 0;
+}
+
+int clientSocket() {
+    // Ottengo il file descriptor del socket
+    int fd_client = socket(AF_UNIX, SOCK_STREAM, 0);
+    // Controllo che sia tutto ok
+    if(fd_client == -1) {
+        perror("SOCKET");
+        return -1;
+    }
+    // Ritento la connessione più volte
+    while(connect(fd_client, (struct sockaddr*)&socket_address, sizeof(socket_address)) == -1) {
+        if(errno == ENOENT) {
+            // riprovo
+            sleep(1);
+        } else {
+            perror("CONNECT");
+            exit(EXIT_FAILURE);
+        }
+    }
+    // Ho il file descriptor del client socket
+    return fd_client;
+}
+
+int readn(long fd, void *buf, size_t size) {
+    size_t left = size;
+    int r;
+    char *bufptr = (char*) buf;
+    while (left > 0) {
+        // Read ritorna il numero di elementi letti.
+        if ((r = read((int)fd, bufptr, left)) == -1) {
+            if (errno = EINTR) continue;
+            return -1;
+        }
+        if (r == 0) {
+            // Sono alla fine del file.
+            return 0; 
+        }
+        // tolgo i byte appena letti
+        left = left -r;
+        // scorro il buffer tanto quanti byte ho appena letto
+        bufptr = bufptr + r;    
+    }
+    return size;
+}
+
+int writen(long fd, void *buf, size_t size) {
+    size_t left = size;
+    int r;
+    char *bufptr = (char*) buf;
+    while(left > 0) {
+        if((r = write((int)fd, bufptr, left)) == -1) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        if(r == 0) {
+            return 0;
+        }
+        left = left -r;
+        bufptr = bufptr - r;
+    }
+    return -1;
+}
