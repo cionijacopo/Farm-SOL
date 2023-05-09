1f84bcfad6e69d55ce6124af66a45e29efab0baa
cionijacopo <cionijacopo4@gmail.com>
2023-05-10 00:18:51 +0200

    Updated master_worker

--- src/client/farm_master_worker.c
+++ src/client/farm_master_worker.c
@@ -8,14 +8,18 @@
 #include"../../includes/utils.h"
 #include"../../includes/threadutils.h"
 #include"../../includes/task_queue.h"
+#include"../../includes/connection.h"
+#include"../../includes/pool.h"
 #include<stdio.h>
 #include<pthread.h>
+#include<unistd.h>
 
 int farm_master(int num_thread, int q_length, int r_time, Node_t *lista) {
     // Prima di tutto devo creare la coda ed inizializzarla
-    TaskQueue_t *safe_queue = (TaskQueue_t *)s_malloc(sizeof(TaskQueue_t));
-    if(initQueue(safe_queue, q_length, r_time) != 0) {
+    TaskQueue_t *safe_queue = initQueue(q_length, r_time);
+    if(safe_queue == NULL) {
         fprintf(stderr, "FATAL ERROR: inizializzazione coda.\n");
+        deleteList(lista);
         return -1;
     }
     
@@ -26,4 +30,45 @@ int farm_master(int num_thread, int q_length, int r_time, Node_t *lista) {
     }
 
     // Comincio ad aggiungiere i files
+    while(lista != NULL) {
+        // Aggiungo l'elemento
+        if(pushPool(safe_queue, lista->info) != 0) {
+            fprintf(stderr, "FATAL ERROR: inserimento elementi nella pool.\n");
+            return -1;
+        }
+        // Scorro la lista di file regolari
+        lista = lista->next;
+    }
+
+    // Avviso della terminazione
+    LOCK(&safe_queue->qlock);
+    safe_queue->uscita++;
+    BCAST(&safe_queue->vuoto);
+    UNLOCK(&safe_queue->qlock);
+
+    // Attendo la terminazione dei threads
+    for(int i = 0; i < num_thread; i++) {
+        JOIN(pool[i]);
+    }
+
+    int fd_master = clientSocket();
+    int length = 3;
+    int n;
+    if((n = writen(fd_master, &length, sizeof(int))) == -1) {
+        fprintf(stderr, "Errore writen master.\n");
+        return -1;
+    }
+    if((n = writen(fd_master, "END", length*sizeof(char))) == -1) {
+        fprintf(stderr, "Errore writen master.\n");
+        return -1;
+    }
+
+    // Chiudo la connessione
+    close(fd_master);
+    // Libero tutta la memoria
+    deleteList(lista);
+    free(safe_queue->coda);
+    free(safe_queue);
+
+    return 0;
 }
