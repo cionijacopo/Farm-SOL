ff20e60ea389c88535b6c3a79eaa4bf9ace40075
cionijacopo <cionijacopo4@gmail.com>
2023-05-10 02:12:35 +0200

    Added finalNodes methods

--- includes/linkedlist.h
+++ includes/linkedlist.h
@@ -8,6 +8,15 @@ typedef struct node {
     struct node *next;
 } Node_t;
 
+typedef struct finalnode {
+    // nome del file
+    char *info;
+    // Risultato
+    long int ris;
+    // Puntatore al nodo successivo
+    struct finalnode *next;
+} FinalNode_t;
+
 /**
  * @function newNode
  * @brief funzione che crea un nuovo nodo
@@ -15,6 +24,15 @@ typedef struct node {
  * @return puntatore al nodo
 */
 Node_t *newNode(char *info);
+
+/**
+ * @function newFinalNode
+ * @brief funzione che crea un nuovo nodo
+ * @param info elemento del nodo
+ * @param ris elemento del nodo
+ * @return puntatore al nodo
+*/
+FinalNode_t *newFinalNode(char *info, long int ris);
 
 /**
  * @function postInsert
@@ -26,12 +44,37 @@ Node_t *newNode(char *info);
 Node_t *postInsert(Node_t *lista, char *info);
 
 /**
+ * @function insertFinalNode
+ * @brief funzione che fa la inser di un nuovo FinalNode in modo ordinato
+ * @param final_list lista a cui aggiungere il nodo
+ * @param info elemento del nodo
+ * @param ris elemento del nodo
+ * @return puntatore alla lista mdoficata
+*/
+FinalNode_t *insertFinalNode(FinalNode_t *final_list, char *info, long int ris);
+
+/**
  * @functio deleteList
  * @brief funzione che elimina la lista
  * @param lista da rimuovere
  * @return NULL se la lista è stata distrutta correttamente
 */
 Node_t *deleteList(Node_t *lista);
+
+/**
+ * @function deleteFinalList
+ * @brief funzione che elimina la lista finale
+ * @param final_list da rimuovere
+ * @return NULL se la lista è stata distrutta correttamente
+*/
+FinalNode_t *deleteFinalList(FinalNode_t *final_list);
+
+/**
+ * @function printFinalList
+ * @brief funzione che stampa la lista
+ * @param final_list lista da stampare
+*/
+void printFinalList(FinalNode_t *final_list);
 
 /**
  * @function countNodes
--- src/core/linkedlist.c
+++ src/core/linkedlist.c
@@ -21,6 +21,18 @@ Node_t *newNode(char *info) {
     return ptr;
 }
 
+FinalNode_t *newFinalNode(char *info, long int ris) {
+    // Alloco lo spazio per il nodo
+    FinalNode_t *ptr = (FinalNode_t *)s_malloc(sizeof(FinalNode_t));
+    // Alloco lo spazio per la stringa
+    ptr->info = (char *)s_malloc(strlen(info)+1);
+    // Copio il valore
+    strncpy(ptr->info, info, strlen(info)+1);
+    ptr->ris = ris;
+    ptr->next = NULL;
+    return ptr;
+}
+
 Node_t *postInsert(Node_t *lista, char *info) {
     if(lista == NULL) {
         return newNode(info);
@@ -30,6 +42,28 @@ Node_t *postInsert(Node_t *lista, char *info) {
     return lista;
 }
 
+FinalNode_t *insertFinalNode(FinalNode_t *final_list, char *info, long int ris) {
+    if(final_list == NULL) {
+        return newFinalNode(info, ris);
+    } else {
+        FinalNode_t *current = final_list;
+        FinalNode_t *new = newFinalNode(info, ris);
+        // Controllo se lo devo inserire direttamente in testa
+        if(current->ris >= new->ris) {
+            new->next = current;
+            final_list = new;
+            return final_list;
+        }
+        // Scorro fino a quando non trovo la posizione giusta
+        while(current->next != NULL && current->next->ris <= new->ris) {
+            current = current->next;
+        }
+        new->next = current->next;
+        current->next = new;
+        return final_list;
+    }
+}
+
 Node_t *deleteList(Node_t *lista) {
     if(lista != NULL) {
         deleteList(lista->next);
@@ -37,6 +71,23 @@ Node_t *deleteList(Node_t *lista) {
         free(lista);
     } else {
         return NULL;
+    }
+}
+
+FinalNode_t *deleteFinalList(FinalNode_t *final_list) {
+    if(final_list != NULL) {
+        deleteFinalList(final_list->next);
+        free(final_list->info);
+        free(final_list);
+    } else {
+        return NULL;
+    }
+}
+
+void printFinalList(FinalNode_t *final_list) {
+    if(final_list != NULL) {
+        fprintf(stdout, "%ld %s\n", final_list->ris, final_list->info);
+        printFinalList(final_list->next);
     }
 }
 
